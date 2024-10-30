7c3d62fea96a14fce58a5a095079aeb75b6d7703
王翊安 <aud986587@gmail.com>
2024-10-30 13:49:50 +0800

    測試monolog

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ require '../vendor/autoload.php';
 // 類別先import進來
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
+
 $lara= new Lara();
 $vincent= new Someone('Vincent');
 // 類別未先import進來
@@ -11,3 +12,15 @@ $john= new Demo\Hello\Someone('John');
 $hello= new Demo\HelloWorld();
 use Demo\HelloWorld as World; // 類別另取別名
 $world = new World();
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
