c79d795c6f3dada9fb162fd6257f8aba1be0ce02
王翊安 <aud986587@gmail.com>
2024-10-30 14:04:02 +0800

    加入carbon程式碼並測試

--- public/index.php
+++ public/index.php
@@ -4,14 +4,14 @@ require '../vendor/autoload.php';
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
 
-$lara= new Lara();
-$vincent= new Someone('Vincent');
+//$lara= new Lara();
+//$vincent= new Someone('Vincent');
 // 類別未先import進來
-$mary= new \Demo\Hello\Someone('Mary');
-$john= new Demo\Hello\Someone('John');
-$hello= new Demo\HelloWorld();
+//$mary= new \Demo\Hello\Someone('Mary');
+//$john= new Demo\Hello\Someone('John');
+//$hello= new Demo\HelloWorld();
 use Demo\HelloWorld as World; // 類別另取別名
-$world = new World();
+//$world = new World();
 
 use Monolog\Level;
 use Monolog\Logger;
@@ -24,3 +24,18 @@ $log->pushHandler(new StreamHandler('E:/wagon160/uwamp/www/auto/log/my.log', Lev
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+
+use Carbon\Carbon;
+
+printf("Right now is %s ", Carbon::now()->toDateTimeString());
+printf("Right now in Vancouver is %s", Carbon::now('America/Vancouver'));  //implicit __toString()
+$tomorrow = Carbon::now()->addDay();
+$lastWeek = Carbon::now()->subWeek();
+
+$officialDate = Carbon::now()->toRfc2822String();
+
+$howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
+
+$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Europe/London');
+
+$internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
